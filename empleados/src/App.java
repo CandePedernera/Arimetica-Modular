0e72e4cc6e127a1116dd08c87ea0cb4677adce3c
NahirC <81126278+CandePedernera@users.noreply.github.com>
2023-05-04 12:47:25 -0300

    Update App.java

--- empleados/src/App.java
+++ empleados/src/App.java
@@ -1,48 +1,19 @@
-import java.time.LocalDate;
-import java.util.Scanner;
+/*import java.time.LocalDate;
+import java.util.Scanner;*/
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner Leer;
-        Empleado emp1=null; 
-        //Empleado [] T; 
+        
         Tabla tablaHash= new Tabla(); 
         //Arr=new Empleado [m];
-        Leer= new Scanner(System.in); 
-        float salario; 
-        String name, puesto; 
-        int op, id; 
-        do {
-            System.out.println("Ingresa una opción");
-            System.out.println("1. Ingresar datos");
-            System.out.println("2. Arimetica mod, insertar y muestra");
-            System.out.println("3. Salir");
-            op= Leer.nextInt(); 
-            Leer.nextLine(); 
-            
-            switch (op) {
-                case 1:
-                    System.out.println("Ingresa el nombre: ");
-                    name= Leer.nextLine(); 
-                    System.out.println("Ingresa el salario: ");
-                    salario= Leer.nextFloat(); 
-                    Leer.nextLine(); 
-                    System.out.println("Ingresa el puesto:");
-                    puesto= Leer.nextLine(); 
-                    System.out.println("Cual es la clave del empleado?");
-                    id= Leer.nextInt(); 
-                    Leer.nextLine(); 
-                    emp1= new Empleado(name, salario, puesto, id); 
-                    break;
-                case 2: 
-                    System.out.println("1. Arimetica modular");
-                    tablaHash.Insert(emp1);
-                    tablaHash.ShowTable();     
-
-                default:
-                    break;
-            }
-
-        } while (op!=3);
-
+        
+        Empleado emp1, emp2, emp3; 
+        emp1= new Empleado("candela", 234, "CEO", 1200); 
+        emp2= new Empleado("Nahir", 234, "CEO", 1500);
+        emp3= new Empleado("Pedernera", 234, "CEO", 1700);
+        System.out.println( "Arimetica modular");
+        tablaHash.Insert(emp1);
+        tablaHash.Insert(emp2);
+        tablaHash.Insert(emp3);
+        tablaHash.MostrarTabla();     
     }
 }
